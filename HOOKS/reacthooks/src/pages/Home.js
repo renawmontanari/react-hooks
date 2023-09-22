de41eb9e58964e09bd1d852fbc924cd76992cf99
renawmontanari <regiangelica@gmail.com>
2023-09-21 23:05:45 -0300

    reactHooks

--- HOOKS/reacthooks/src/pages/Home.js
+++ HOOKS/reacthooks/src/pages/Home.js
@@ -3,12 +3,14 @@ import React from 'react'
 // Components
 import HookUseState from '../components/HookUseState';
 import HookUseReducer from '../components/HookUseReducer';
+import HookUseEffects from '../components/HookUseEffects';
 
 const Home = () => {
   return (
     <div>
         <HookUseState />
         <HookUseReducer />
+        <HookUseEffects />
     </div>
   );
 };
