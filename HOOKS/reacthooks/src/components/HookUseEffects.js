b5943f143bcdf596e1ab3b6a994a7ea536b78691
renawmontanari <regiangelica@gmail.com>
2023-09-25 23:55:53 -0300

    reactHooks

--- HOOKS/reacthooks/src/components/HookUseEffects.js
+++ HOOKS/reacthooks/src/components/HookUseEffects.js
@@ -15,16 +15,27 @@ const HookUseEffects = () => {
 
     // 2- Array de deps. vazio
     useEffect(() => {
-
       console.log("Serei executado apenas uma");
-
     }, []);
+
+    // 3- Item no array de deps.
+    const [anotherNumber, setAnotherNumber] = useState(0);
+
+    useEffect(() => {
+      if (anotherNumber > 0) {
+        console.log("Sou executado apenas quando o anotherNumber muda!");
+      }
+    }, [anotherNumber]);
 
   return (
     <div>
         <h2>useEffect</h2>
         <p>Number: {number}</p>
         <button onClick={changeSomething}>Executar</button>
+        <p>Another Number: {anotherNumber}</p>
+        <button onClick={() => setAnotherNumber(anotherNumber + 1)}>
+          Mudar AnotherNumber!
+        </button>
         <hr />
     </div>
   );
